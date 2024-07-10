d9f6255e3df909c0d8752a64050b574730a754e9
Fajar Windhu Zulfikar <fajarwindhuzulfikar@gmail.com>
2024-07-10 09:17:37 +0700

    init

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
+            'id_card_number' => $idCardNumber = Str::random(16),
+            'driving_license_number' => $drivingLicenseNumber = Str::random(7),
+            // 'id_card_number_hash' => $idCardNumber,
+            // 'driving_license_number_hash' => $drivingLicenseNumber,
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
